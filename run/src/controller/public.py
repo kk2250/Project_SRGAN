853d84e0b7c3479735d65960025bb66e97407f05
Kenny <xclen@hotmail.com>
2018-12-13 15:14:47 -0500

    new update

--- run/src/controller/public.py
+++ run/src/controller/public.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Blueprint,render_template,request,session,redirect,url_for
+from flask import Blueprint,render_template,request,session,redirect,url_for,send_file
+from werkzeug import secure_filename
+import os
 # from model import model
 
 # UPLOAD_FOLDER = '/Users/kkim2250/Desktop/Project_SRGAN/'
@@ -8,19 +10,28 @@ from flask import Blueprint,render_template,request,session,redirect,url_for
 
 controller = Blueprint('public',__name__)
 # controller.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# controller.config = {}
+
+# @controller.record
+# def record_params(setup_state):
+#   app = setup_state.app
+#   controller.config = dict([(key,value) for (key,value) in app.config.items()])
 
 @controller.route('/',methods=['GET','POST'])
 def frontpage():
     if request.method == 'GET':
         return render_template('homepage.html')
     elif request.method == 'POST':
-        img_file = request.file['image']
-        if img_file:
+        if request.files['image']:
+            img_file = request.files['image']
             filename = secure_filename(img_file.filename)
-            img_file.save(os.path.join("image1.png"))
-            return render_template('printpage.html', message=message)
+            img_file.save(os.path.join("run/src/static/image1.png"))
+            # with open('run/src/static/image1.png') as f:
+            return render_template('printpage.html')
 
-
+@controller.route('/download')
+def download():
+    return send_file('/Users/kkim2250/Desktop/Project_SRGAN/run/src/static/image1.png', as_attachment=True, attachment_filename="image1.png")
 
 
 # @controller.route('/out',methods=['GET','POST'])
